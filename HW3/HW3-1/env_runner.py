885f5dd17851dbb4c434d7824d50495fbea7ee06
Te-Wei Ko <229563995+TeWei02@users.noreply.github.com>
2026-03-26 06:28:40 +0000

    HW3: complete TODO 1-4 (PPO model, env_runner, clipped loss) and TODO 6 (reward design + safety shield for Proly)

--- HW3/HW3-1/env_runner.py
+++ HW3/HW3-1/env_runner.py
@@ -88,15 +88,21 @@ class EnvRunner:
             # values      : (n_env)
             # rewards     : (n_env)
             # TODO 3: Run a step to collect data
-            """
-            self.mb_states[step, :]  = ...
-            self.mb_dones[step, :]   = ...
-            self.mb_actions[step, :] = ...
-            self.mb_a_logps[step, :] = ...
-            self.mb_values[step, :]  = ...
-            self.states, rewards, self.dones, info = self.env.step(actions)
-            self.mb_rewards[step, :] = ...
-            """
+            self.mb_states[step, :]  = self.states
+            self.mb_dones[step, :]   = self.dones
+
+            states_tensor = torch.from_numpy(self.states).float().to(self.device)
+            with torch.no_grad():
+                actions, a_logps = policy_net(states_tensor)
+                values = value_net(states_tensor)
+
+            actions_np = actions.cpu().numpy()
+            self.mb_actions[step, :] = actions_np
+            self.mb_a_logps[step, :] = a_logps.cpu().numpy()
+            self.mb_values[step, :]  = values.cpu().numpy()
+
+            self.states, rewards, self.dones, info = self.env.step(actions_np)
+            self.mb_rewards[step, :] = rewards
 
         last_values = value_net(torch.from_numpy(self.states).float().to(self.device)).cpu().numpy()
         self.record()
